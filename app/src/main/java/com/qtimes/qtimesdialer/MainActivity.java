16119eea02f86a3c8c409165c1d0517636f3ff8c
JackHou <jackhou1990@163.com>
2020-02-10 17:52:29 +0800

    添加广播

--- app/src/main/java/com/qtimes/qtimesdialer/MainActivity.java
+++ app/src/main/java/com/qtimes/qtimesdialer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import im.dlg.dialer.DialpadActivity;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,6 +17,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tvNum;
+    private static final String BROADCAST_PERMISSION_DISC = "com.qtimes.permissions.DIALER_BROADCAST";
+    private static final String BROADCAST_ACTION_DISC = "com.qtimes.permissions.dialer_broadcast";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +40,10 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Main", formatted + ",  " + raw);
             tvNum.setText(raw);
             if ("1234".equals(raw)) {
-                Intent intent = new Intent("com.qtimes.dialer");
-                intent.putExtra("num", raw);
-                sendBroadcast(intent);
+                Intent intent = new Intent();  //Itent就是我们要发送的内容
+                intent.putExtra("host", raw);
+                intent.setAction(BROADCAST_ACTION_DISC);   //设置你这个广播的action，只有和这个action一样的接受者才能接受者才能接收广播
+                sendBroadcast(intent, BROADCAST_PERMISSION_DISC);   //发送广播
             }
         }
     }
